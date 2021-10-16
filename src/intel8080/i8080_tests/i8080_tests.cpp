9bce4260a8576a2e1423c78a6ba56cd15779e1d6
VitorMob <mobhacking100@gmail.com>
2021-10-16 19:22:22 -0300

    new test added

--- src/intel8080/i8080_tests/i8080_tests.cpp
+++ src/intel8080/i8080_tests/i8080_tests.cpp
@@ -4,6 +4,7 @@
 // if you wanted to send a pull stay a I will analyze them all
 
 #include "i8080.hpp"
+#include <time.h>
 
 void execute_tests(std::string name)
 {
@@ -17,12 +18,12 @@ void execute_tests(std::string name)
     byte_t C = 0;
 
     cpu.load_file_bin(name, mem, 0x100); // load bin for memory and jump pc for 0x100
-    int instructions = 0;
+    int instructions = 0;                // counter instructions
 
-    mem[5] = 0xc9; // inject memory for subrotine
-        
-    while (instructions < 100000000)
-    { 
+    mem[5] = 0xc9;
+
+    while (true)
+    {
         PC = cpu.get_pc();
         E = cpu.get_register_e();
         C = cpu.get_register_c();
@@ -31,14 +32,10 @@ void execute_tests(std::string name)
         if (PC == 5)
         {
             if (C == 2)
-            {
                 std::cout.put(E);
-            }
             if (C == 9)
-            {
                 for (int i = DE; mem[i] != '$'; i++)
                     std::cout.put(mem[i]);
-            }
         }
 
         cpu.i8080_instructions();
@@ -46,17 +43,23 @@ void execute_tests(std::string name)
 
         if (PC == 0)
             break;
-
-        if (mem[PC] == 0x76) // not implemented hlt
-            break;
     }
-    std::cout << "\n\n*** Cycles=" << cpu.get_cycles() << " Instructions=" << instructions << std::endl
+    std::cout << "\n\n*** Cycles=" << cpu.get_cycles() << std::endl
+              << "*** Instructions=" << instructions << std::endl
               << std::endl;
 }
 
 int main()
 {
-    execute_tests("src/intel8080/i8080_tests/bin/TEST.COM");
+
+    clock_t start = clock();
+
+    execute_tests("src/intel8080/i8080_tests/bin/TST8080.COM");
+    execute_tests("src/intel8080/i8080_tests/bin/8080PRE.COM");
+
+    clock_t result = clock() - start;
+
+    std::cout << "*** runtime=" << result << " ms" << std::endl;
 
     return EXIT_SUCCESS;
 }
